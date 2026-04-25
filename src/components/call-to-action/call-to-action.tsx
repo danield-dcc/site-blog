6cea01aa3005a642cd0a2c365464ca447d2777f1
Daniel <danieldcc.colares@gmail.com>
2026-04-25 17:32:36 -0300

    fix: change how fonts are loaded

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -1,12 +1,6 @@
 import { ArrowRight, Store } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { PT_Sans_Caption } from "next/font/google";
-
-const ptSansCaption = PT_Sans_Caption({
-  subsets: ["latin"],
-  weight: "700",
-});
 
 export function CallToAction() {
   return (
@@ -17,9 +11,7 @@ export function CallToAction() {
             <Store className="text-cyan-100" />
           </div>
 
-          <h2
-            className={`${ptSansCaption.className} text-gray-100 text-balance text-heading-xl`}
-          >
+          <h2 className="text-gray-100 text-balance text-heading-xl font-sans">
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
 
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,22 +1,33 @@
-import { ReactNode } from "react"
-import Header from "../header/header"
-import Footer from "../footer/footer"
-import { Inter } from "next/font/google"
+import { ReactNode } from "react";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import { Inter, PT_Sans_Caption } from "next/font/google";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-inter",
+});
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-sans",
+});
 
 export function Layout({ children }: LayoutProps) {
   return (
     <div
-      className={`${inter.className} relative flex min-h-screen flex-col dark`}
+      className={`${ptSansCaption.variable} ${inter.variable}
+      relative flex min-h-screen flex-col dark font-inter `}
     >
       <Header />
-      <main className="flex-1 flex flex-col mb-12 ">{children}</main>
+      <main className="flex-1 flex flex-col mt-10 mb-12 ">{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
