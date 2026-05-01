b6f670c41d5536abed29126618d48183e27ccb1a
Daniel <danieldcc.colares@gmail.com>
2026-05-01 12:26:55 -0300

    feat:  adds ssg

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -2,23 +2,27 @@ import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
 import { PostGridCard } from "./components/post-grid-card";
-import { allPosts } from "contentlayer/generated";
+import { Post } from "contentlayer/generated";
 import { Inbox } from "lucide-react";
 
-export function BlogList() {
+export interface BlogListProps {
+  posts: Post[];
+}
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
     ? `Resultados de busca para: "${query}"`
     : "Dicas e estratégias para impulsionar o seu negócio";
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postList = query
+    ? posts.filter((post) =>
         post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase()),
       )
-    : allPosts;
+    : posts;
 
-  const hasPosts = posts.length > 0;
+  const hasPosts = postList.length > 0;
 
   return (
     <div className="flex flex-col py-24 grow h-full">
@@ -40,7 +44,7 @@ export function BlogList() {
 
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
--- src/templates/blog/post-page.tsx
+++ src/templates/blog/post-page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { allPosts } from "contentlayer/generated";
+import { Post } from "contentlayer/generated";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -14,15 +13,14 @@ import { MarkDown } from "@/components/markdown";
 import { Button } from "@/components/ui/button";
 import { useShare } from "@/hooks";
 
-export function PostPage() {
-  const router = useRouter();
-  const slug = router.query.slug as string;
-  const post = allPosts.find(
-    (post) => post?.slug?.toLocaleLowerCase() === slug?.toLocaleLowerCase(),
-  )!;
+export interface PostPageProps {
+  post: Post;
+}
+
+export function PostPage({ post }: PostPageProps) {
   const publishedDate = new Date(post?.date).toLocaleDateString("pt-BR");
 
-  const postUrl = `https://site.set/blog/${slug}`;
+  const postUrl = `https://site.set/blog/${post.slug}`;
 
   const { shareButtons } = useShare({
     url: postUrl,
